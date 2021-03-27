91bac1d6ef33343d35f5a52d91cb560e0097301e
Alex-Jer <alexandrejeronimo2001@gmail.com>
2021-03-27 22:47:46 +0000

    output em txt, validações, outputs extra

--- word_stats.sh
+++ word_stats.sh
@@ -11,6 +11,9 @@ fi
 
 filepath="$2"
 filename="$(basename "$2")"
+filenameNoExt="${filename%.*}"
+stopwordsPath="StopWords/$3.stop_words.txt"
+stopwordsLang="$3"
 
 # Validação do caminho do ficheiro
 if ! test -f "$filepath"; then
@@ -34,18 +37,32 @@ fi
 
 echo >&2 "[INFO] Processing '$filename'"
 
-# Validação da existência de Stop Words e output do ranking de palavras
-if [ "$1" == 'c' ] || [ "$1" == 'p' ] || [ "$1" == 't' ]; then
+# Validação da existência de Stop Words
+[ "$1" == 'c' ] || [ "$1" == 'p' ] || [ "$1" == 't' ] && removeStopWords=true
+
+if [ "$removeStopWords" = true ]; then
   echo >&2 "[INFO] STOP WORDS will be filtered out"
-  if [ "$3" == "pt" ]; then
-    echo >&2 "Stop Words file 'pt':"
+  # Se o ficheiro de Stop Words estiver em português
+  if [ "$stopwordsLang" == "pt" ]; then
+    echo >&2 "Stop Words file 'pt': '$stopwordsPath' ($(wc -l "$stopwordsPath" | cut -d'S' -f1)words)"
+  # Se o ficheiro de Stop Words estiver em inglês
   else
     echo >&2 "Stop Words file 'en':"
   fi
+  # Criação do ficheiro com o ranking das palavras sem StopWords
   echo " COUNT MODE"
-  tr '.' ' ' <"$2" | tr -s ' ' '\n' | grep -vwf ./StopWords/"$3".stop_words.txt | sort | uniq -c | sort -r | cut -c 5- | nl
+  tr -d 0-9 <"$filename" | tr -d '[:punct:]' | tr -s ' ' '\n' | grep -vwif "$stopwordsPath" |
+    sort | uniq -c | sort -r | cut -c 5- | nl >&1 | tee result---"$filenameNoExt".txt
 else
+  # Criação do ficheiro com o ranking das palavras com StopWords
   echo >&2 "[INFO] STOP WORDS will be counted"
   echo " COUNT MODE"
-  tr '.' ' ' <"$2" | tr -s ' ' '\n' | sort | uniq -c | sort -r | cut -c 5- | nl
+  tr -d 0-9 <"$filename" | tr -d '[:punct:]' | tr -s ' ' '\n' | tr -d ' ' |
+    sort | uniq -c | sort -r | cut -c 5- | nl >&1 | tee result---"$filenameNoExt".txt
 fi
+
+# Output do número total de palavras e dos detalhes do ficheiro gerado
+totalWords=$(wc -l result---"$filenameNoExt".txt | cut -d'r' -f1)
+echo "RESULTS: 'result---$filenameNoExt.txt'" &&
+  ls -al result---"$filenameNoExt".txt
+echo "$totalWords distinct words"
