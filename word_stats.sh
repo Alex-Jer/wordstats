59c71dcca870f2aca22fb99482a5d21f629f7dea
Alex-Jer <alexandrejeronimo2001@gmail.com>
2021-04-02 21:16:00 +0100

    Tradução dos comentários para inglês e gnuplot

--- word_stats.sh
+++ word_stats.sh
@@ -1,64 +1,62 @@
 #!/usr/bin/env bash
 
-# Validação de parâmetros
+# Arguments validation
 if ! [ "$1" ] || ! [ "$2" ]; then
   echo >&2 "[ERROR] Insufficient parameters!"
   echo >&2 "./word_stats.sh Cc|Pp|Tt INPUT [iso3166]"
   exit 1
 fi
 
-# set -u # É obrigatório usar?
-
 mode="$1"
 filepath="$2"
 filename="$(basename "$2")"
 filenameNoExt="${filename%.*}"
 
-# Se o utilizador especificar a língua das Stop Words
+# If the user specifies the Stop Words language
 if [ "$3" ]; then
   stopwordsLang="$3"
 else
-  # Se não especificar, assume inglês
+  # Assumes English by default
   stopwordsLang="en"
 fi
 
 stopwordsPath="StopWords/$stopwordsLang.stop_words.txt"
 
-# Validação do caminho do ficheiro
+# File path validation
 if ! test -f "$filepath"; then
   echo >&2 "[ERROR] File '$filename' not found!"
   exit 1
 fi
 
-# Validação de tipo de ficheiro
+# File type validation
 if [[ $filename == *.txt ]]; then
   echo >&2 "'$filename': Text file"
 else
-  # Cria um novo ficheiro de texto a partir do PDF temporariamente
+  # Makes a new temporary text file from the PDF file
   echo >&2 "'$filename': PDF file"
   isPdf=true
   pdftotext "$filepath" "temp---$filenameNoExt".txt
   filepath="temp---$filenameNoExt".txt
 fi
 
-# Validação da existência de Stop Words
+# Stop Words validation
 if [ "$mode" == 'c' ] || [ "$mode" == 'p' ] || [ "$mode" == 't' ]; then
   echo >&2 "[INFO] STOP WORDS will be filtered out"
   case "$stopwordsLang" in
-  pt) # Ficheiro de Stop Words em português
+  pt) # Portuguese Stop Words file
     echo >&2 "Stop Words file 'pt': '$stopwordsPath' ($(wc -l "$stopwordsPath" | cut -d'S' -f1)words)"
     ;;
-  en) # Ficheiro de Stop Words em inglês
+  en) # English Stop Words file
     echo >&2 "Stop Words file 'en': '$stopwordsPath' ($(wc -l "$stopwordsPath" | cut -d'S' -f1)words)"
     ;;
-  *) # Língua inválida
+  *)
     echo "[ERROR] Invalid language"
     exit 1
     ;;
   esac
 fi
 
-# Output do número total de palavras e dos detalhes do ficheiro gerado
+# Output the number of distinct words and the details of the "result" file
 details_output() {
   totalWords=$(wc -l <result---"$filenameNoExt".txt)
   case "$mode" in
@@ -67,22 +65,19 @@ details_output() {
       ls -al result---"$filenameNoExt".txt
     echo "$totalWords distinct words"
     ;;
-  p | P)
-    #
-    ;;
-  t | T)
-    # Se a variável de ambiente WORD_STATS_TOP existir mas não for um número inteiro positivo
+  *)
+    # If the WORD_STATS_TOP environment variable exists but isn't a positive integer
     if ! [[ -z "$WORD_STATS_TOP" || ($WORD_STATS_TOP =~ ^[0-9]+$) && ($WORD_STATS_TOP -gt 0) ]]; then
       top=10
       echo "'$WORD_STATS_TOP' not a positive number (using default 10)"
       echo "WORD_STATS_TOP=$top"
     else
-      # Se a variável de ambiente WORD_STATS_TOP não existir
+      # If the WORD_STATS_TOP environment variable doesn't exist
       if ! [ "$WORD_STATS_TOP" ]; then
         top=10
         echo "Environment variable 'WORD_STATS_TOP' is empty (using default 10)"
         echo "WORD_STATS_TOP=$top"
-      # Se a variável de ambiente WORD_STATS_TOP estiver correta
+      # If the WORD_STATS_TOP environment variable is correct
       else
         top=$WORD_STATS_TOP
         echo "WORD_STATS_TOP=$top"
@@ -95,47 +90,58 @@ details_output() {
 }
 
 case "$mode" in
-c) # Contagem de cada palavra sem Stop Words
+c) # Count words excluding Stop Words
   echo >&2 "[INFO] Processing '$filename'"
   grep -oE '[[:alpha:]]*' <"$filepath" | tr -s ' ' '\n' | grep -vwif "$stopwordsPath" | sort | uniq -c |
-    sort -r | cut -c 5- | nl >result---"$filenameNoExt".txt
+    sort -nr | cut -c 5- | nl >result---"$filenameNoExt".txt
   details_output
   ;;
-C) # Contagem de cada palavra com Stop Words
+C) # Count words including Stop words
   echo >&2 "[INFO] Processing '$filename'"
   grep -oE '[[:alpha:]]*' <"$filepath" | tr -s ' ' '\n' | tr -d ' ' | sort | uniq -c |
-    sort -r | cut -c 5- | nl >result---"$filenameNoExt".txt
+    sort -nr | cut -c 5- | nl >result---"$filenameNoExt".txt
   details_output
   ;;
-p) # Gráfico da contagem de cada palavra sem Stop Words
+p) # Bar graph of the top WORD_STATS_TOP words excluding Stop Words
   echo >&2 "[INFO] Processing '$filename'"
+  details_output
+  grep -oE '[[:alpha:]]*' <"$filepath" | tr -s ' ' '\n' | grep -vwif "$stopwordsPath" | sort | uniq -c |
+    sort -nr | cut -c 5- | head -n "$top" | nl >result---"$filenameNoExt".txt
+  # gnuplot -e "filepath='$filename.png'" bar.gp
+  gnuplot <bar.gp
+  # display out.png
   ;;
-P) # Gráfico da contagem de cada palavra com Stop Words
+P) # Bar graph of the top WORD_STATS_TOP words including Stop Words
   echo >&2 "[INFO] Processing '$filename'"
+  details_output
+  grep -oE '[[:alpha:]]*' <"$filepath" | tr -s ' ' '\n' | tr -d ' ' | sort | uniq -c |
+    sort -nr | cut -c 5- | nl | head -n "$top" >result---"$filenameNoExt".txt
+  gnuplot <bar.gp
+  # display out.png
   ;;
-t) # Top N da contagem de cada palavra sem Stop Words
+t) # Top WORD_STATS_TOP words excluding Stop Words
   echo >&2 "[INFO] Processing '$filename'"
   details_output
   echo "-------------------------------------"
   echo "# TOP $top elements"
   grep -oE '[[:alpha:]]*' <"$filepath" | tr -s ' ' '\n' | grep -vwif "$stopwordsPath" | sort | uniq -c |
-    sort -r | cut -c 5- | nl | head -n "$top" | tee result---"$filenameNoExt".txt
+    sort -nr | cut -c 5- | head -n "$top" | nl | tee result---"$filenameNoExt".txt
   echo "-------------------------------------"
   ;;
-T) # Top N da contagem de cada palavra com Stop Words
+T) # Top WORD_STATS_TOP words including Stop Words
   echo >&2 "[INFO] Processing '$filename'"
   details_output
   echo "-------------------------------------"
   echo "# TOP $top elements"
   grep -oE '[[:alpha:]]*' <"$filepath" | tr -s ' ' '\n' | tr -d ' ' | sort | uniq -c |
-    sort -r | cut -c 5- | nl | head -n "$top" | tee result---"$filenameNoExt".txt
+    sort -nr | cut -c 5- | nl | head -n "$top" | tee result---"$filenameNoExt".txt
   echo "-------------------------------------"
   ;;
-*) # Comando inválido
+*)
   echo >&2 "[ERROR] Unknown command '$mode'"
   exit 1
   ;;
 esac
 
-# Apaga o ficheiro temporário criado pelo pdftotext
+# Deletes the temporary file created by pdftotext
 if [ $isPdf ]; then rm "$filepath"; fi
